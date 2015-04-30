4b42320101d7f1430f1350a410df1052c7d7142a
Isabella Li <dnflyangle@gmail.com>
2015-04-30 17:24:34 +1000

    add spec_helper for all tests.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'rack/test'
+require 'rspec'
+require 'capybara'
+
+require File.expand_path '../../index.rb', __FILE__
+
+ENV['RACK_ENV'] = 'test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
