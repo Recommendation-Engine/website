2a67d3ac02823802aaf2bfbae900867fb3baf4e9
Isabella Li <dnflyangle@gmail.com>
2015-10-21 17:23:48 +1100

    Using handlebar to replace the ugly JQuery HTML Injection. yay!

--- public/javascript/mega-logic.js
+++ public/javascript/mega-logic.js
@@ -1,18 +1,10 @@
 $(function() {
+    var templates = {}
+
     function word_cloud(data) {
         var fill = d3.scale.category20();
-        d3.layout.cloud().size([300, 300])
-            .words(data.map(function(d) {
-                return {text: d.name, size: d.weight*2+10};
-            }))
-            .rotate(function() { return ~~(Math.random() * 2) * 90; })
-            .font("Impact")
-            .fontSize(function(d) { return d.size; })
-            .on("end", draw)
-            .start();
-
-        function draw(words) {
-            d3.select(".user-details").append("svg")
+        var draw = function(words) {
+            d3.select(".interest").append("svg")
                 .attr("width", 300)
                 .attr("height", 300)
                 .append("g")
@@ -28,30 +20,17 @@ $(function() {
                   return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
                 .text(function(d) { return d.text; });
-          }
-    }
-    function render_movie_details(element, classname) {
-        var imageUrl = (element.imgUrl == "N/A") ? "assets/movie-not-found.png" : element.imgUrl;
-        $("<div class='card small-card'>" +
-            "<div class='image'>" +
-            "<img src=" + imageUrl + "/>" +
-          "</div>" +
-          "<div class='content'>" + 
-            "<div class='header'>" + element.title + "</div>" +
-            "<div class='description' align=left>" +
-                "<div><B>Ratings: </B>" + element.rating + "</div>"
-                + "<div><B>Director: </B>" + element.director + "</div>"
-                + "<div><B>Actors: </B>" + element.actors + "</div>" +
-                "<div><B>Genre: </B>" + element.genre + "</div>" +
-          "</div>" + "</div>").appendTo(classname);
-    }
+        }
 
-    function render_movies(movies) {
-        $('.card-list').empty();
-
-        $.each(movies, function (index, element) {
-            render_movie_details(element, '.card-list');
-        });
+        d3.layout.cloud().size([300, 300])
+            .words(data.map(function(d) {
+                return {text: d.name, size: d.weight*2+10};
+            }))
+            .rotate(function() { return ~~(Math.random() * 2) * 90; })
+            .font("Impact")
+            .fontSize(function(d) { return d.size; })
+            .on("end", draw)
+            .start();
     }
 
     function active_user_search(){
@@ -68,52 +47,85 @@ $(function() {
         var selectedMovieId = $('#movie-selection').val();
 
         $.get('/movies/' + selectedMovieId, function(data){
-            var imageUrl = (data.selected_movie.imgUrl == "N/A") ? "assets/movie-not-found.png" : data.selected_movie.imgUrl;
-            $('.search-creteria').empty();
-            $('.search-creteria').append("<div class='ui card movie-detail-card'></div>");
-            $('.movie-detail-card').append("<div class='image'>" +
-                                                "<img src=" + imageUrl + "/>" +
-                                            "</div>" + 
-                                            "<div class='content movie-details'>" + 
-                                                "<div class='header'>" + data.selected_movie.title + " </div>" + 
-                                            "</div>");
-            $('.movie-details').append("<div class='discription'>" + 
-                                          "<div> <B>Rating: </B>" + data.selected_movie.rating + "</div>" 
-                                        + "<div> <B>Director: </B>" + data.selected_movie.director + "</div>"
-                                        + "<div> <B>Actors: </B>" + data.selected_movie.actors + "</div>"
-                                        + "<div> <B>Genre: </B>" + data.selected_movie.genre + "</div>" +
-                                        "</div>" );
-
-            render_movies(data.top_5_movies);
+            renderMovie(data.selected_movie);
+            renderMovies(data.top_5_movies);
         });
     });
 
     $('#user-selection').change(function() {
         var selectedUserId = $('#user-selection').val();
         $.get('/users/' + selectedUserId, function(data){
-            var imageUrl = '';
-            if(data.selected_user.gender == 'F')
-                imageUrl = "assets/Female.png";
-            else
-                imageUrl = "assets/Male.png";
-            $('.search-creteria').empty();
-            $('.search-creteria').append("<div class='ui card user-detail-card'></div>");
-            $('.user-detail-card').append("<div class='image'>" +
-                                                "<img src=" + imageUrl + "/>" +
-                                            "</div>" +
-                                            "<div class='content user-details'>" + 
-                                            "<div class='header'> Selected User </div>" + 
-                                          "</div>");
-            $('.user-details').append("<div> <B>Age: </B>" + data.selected_user.age + "</div>" 
-                                    + "<div> <B>Gender: </B>" + data.selected_user.gender + "</div>" 
-                                    + "<div> <B>Zip Code: </B>" + data.selected_user.zipcode + "</div>" 
-                                    + "<div> <B>Occupation: </B>" + data.selected_user.occupation + "</div>"
-                                    + "<div> <B>Interests: </B></div>");
-            word_cloud(data.selected_user.genre)
+            renderUser(data.selected_user);
+            
+            //word_cloud(data.selected_user.genre)
 
-            render_movies(data.top_5_movies)
+            renderMovies(data.top_5_movies)
         });
     });
+
+    function renderUser(user) {
+        if (templates['user']) {        
+            $('.search-creteria').html(templates['user'](user));
+            word_cloud(user.genre)
+        } else {
+            $.get("/javascript/templates/user.handlebar", function(template_source) {
+                Handlebars.registerHelper('imgSrc', function(gender) {
+                    if (gender == 'F') {
+                        return "/assets/Female.png";
+                    } else {
+                        return "/assets/Male.png"
+                    }
+                });
+
+                templates['user'] = Handlebars.compile(template_source);
+                $('.search-creteria').html(templates['user'](user));
+                word_cloud(user.genre)
+            });            
+        }
+    }
+
+    function defineMovieHelpers() {
+        Handlebars.registerHelper('movieUrl', function(movieUrl) {
+            if (movieUrl == 'N/A') {
+                return "/assets/movie-not-found.png";
+            } else {
+                return movieUrl;
+            }
+        });
+    }
+
+    function renderMovie(movie) {   
+        var renderUsing = function(template) {
+            $('.search-creteria').html("<div class='ui card movie-detail-card'>" + 
+                                            template(movie) +
+                                            "</div>");
+        }        
+        if (templates['movie']) {        
+            renderUsing(templates['movie']);
+        } else {
+            $.get("/javascript/templates/movie.handlebar", function(source) {
+                defineMovieHelpers();
+                templates['movie'] = Handlebars.compile(source);
+                renderUsing(templates['movie']);
+            });            
+        }
+    }
+
+    function renderMovies(movies) {
+        if (templates['movies']) {
+            $(".movie-list").html(templates['movies'](movies));
+        } else {
+            $.get("/javascript/templates/movie-list.handlebar", function(template_source) {
+                $.get("/javascript/templates/movie.handlebar", function(partial_source) {
+                    defineMovieHelpers();
+                    Handlebars.registerPartial('movieTemplate', partial_source);
+                    templates['movies'] = Handlebars.compile(template_source);
+                    $(".movie-list").html(templates['movies'](movies));
+                });
+            });
+        }
+        
+    }
 
     $('#movie').click(function() {
         $('#user').removeClass('active');
